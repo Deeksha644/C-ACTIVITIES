2c9420d9760803c38fd3c5ccb73e3bb70b9d64b1
Deeksha644 <72297197+Deeksha644@users.noreply.github.com>
2021-02-08 16:03:08 +0530

    Update ACTIVITY-4.C

--- ACTIVITY-4.C
+++ ACTIVITY-4.C
@@ -7,23 +7,16 @@ float input()
     float number ;
     printf("Enter number:");
     scanf("%f",&number);
-    return number;
-    
+    return number;  
 }
-
-
 float add(float num1, float num2)
 {
     return num1+num2;
 }
-
-
 void output(float n1, float n2, float sum)
 {
     printf("Sum of %f and %f is : %f",n1,n2,sum);
 }
-
-
 int main()
 {
     float x,y,z;
