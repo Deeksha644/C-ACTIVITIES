685a32333773ec017cb55004b8d27bb8234939b1
Deeksha644 <72297197+Deeksha644@users.noreply.github.com>
2021-02-08 14:45:19 +0530

    Update and rename README.md to ACTIVITY-4.C

---
+++ ACTIVITY-4.C
@@ -0,0 +1,36 @@
+# PROGRAM TO ADD TWO NUMBERS USING FOUR FUNCTIONS
+
+#include <stdio.h>
+
+float input()
+{
+    float number ;
+    printf("Enter number:");
+    scanf("%f",&number);
+    return number;
+    
+}
+
+
+float add(float num1, float num2)
+{
+    return num1+num2;
+}
+
+
+void output(float n1, float n2, float sum)
+{
+    printf("Sum of %f and %f is : %f",n1,n2,sum);
+}
+
+
+int main()
+{
+    float x,y,z;
+    x=input();
+    y=input();
+    z=add(x,y);
+    output(x,y,z);
+    return 0;
+    
+}
