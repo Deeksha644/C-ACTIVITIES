c5a225528d902aeb0154e201da37fa718d5c5e92
Deeksha644 <72297197+Deeksha644@users.noreply.github.com>
2021-02-08 17:51:57 +0530

    initial commit

---
+++ ACTIVITY-5.c
@@ -0,0 +1,26 @@
+#Program to calculate the volume of tromboloid using four functions
+
+#include <stdio.h>
+
+float input()
+{   
+    float a;
+    printf("Enter the value:");
+    scanf("%f",&a);
+    return a;
+}
+float volume(float a, float b, float c)
+{
+    float volume=(float)1/(float)3*((a*b*c)+(c/b));
+    return volume;
+}
+int main()
+{
+    float h,b,d,V;
+    h=input();
+    b=input();
+    d=input();
+    V=volume(h,b,d);
+    printf("Volume of tromboloid is : %f",V);
+    return 0;
+}
