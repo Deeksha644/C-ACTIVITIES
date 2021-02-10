15b94cdaba8b6f6da3adbd03c21d20189a5b391d
Deeksha644 <72297197+Deeksha644@users.noreply.github.com>
2021-02-10 22:51:10 +0530

    Update program.c

--- program.c
+++ program.c
@@ -1,28 +1,24 @@
-#include<stdio.h>
-int gcd(int a, int b) 
-{ 
-    if (a == 0) 
-        return b; 
-    return gcd(b%a, a); 
-} 
-void lowest(int den3, int num3) 
-{ 
-    int a,b;
-    int common_factor = gcd(den3, num3);
-    den3 = den3/common_factor; 
-    num3 = num3/common_factor; 
-} 
-void addFraction(int num1, int den1, int num2, int den2, int num3, int den3) 
-{ 
-    den3 = gcd(den1,den2); 
-    den3 = (den1*den2) / den3; 
-    num3 = (num1)*(den3/den1) + (num2)*(den3/den2);
-    lowest(den3,num3); 
-} 
-int main() 
-{ 
-    int num1=1, den1=4, num2=2, den2=4, den3, num3; 
-    addFraction(num1, den1, num2, den2, num3, den3); 
-    printf("%d/%d + %d/%d is equal to %d/%d\n", num1, den1,num2, den2, num3, den3); 
-    return 0; 
-} 
+#include <stdio.h>
+int gcd(int a, int b) {
+   if (a == 0)
+      return b;
+   return gcd(b%a, a);
+}
+void smallest(int &den3, int &n3) {
+   int common_factor = gcd(n3,den3);
+   den3 = den3/common_factor;
+   n3 = n3/common_factor;
+}
+void add_frac(int n1, int den1, int n2, int den2, int &n3, int &den3) {
+ 
+   den3 = gcd(den1,den2);
+   den3 = (den1*den2) / den3;
+   n3 = (n1)*(den3/den1) + (n2)*(den3/den2);
+   smallest(den3,n3);
+}
+int main() {
+   int n1=1, den1=4, n2=2, den2=12, den3, n3;
+   add_frac(n1, den1, n2, den2, n3, den3);
+   printf("%d/%d + %d/%d = %d/%d\n", n1, den1, n2, den2, n3, den3);
+   return 0;
+}
